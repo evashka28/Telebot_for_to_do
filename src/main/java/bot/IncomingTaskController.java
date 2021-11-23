784ecf0bd1890192cbe3bff36870ad55a910db42
distortedroom <nikitos1151@gmail.com>
2021-11-23 21:12:53 +0300

    Task and IncomingTaskController for incoming scheduled tasks

--- src/main/java/bot/IncomingTaskController.java
+++ src/main/java/bot/IncomingTaskController.java
@@ -1,12 +1,9 @@
 package bot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class IncomingTaskController {
-    @GetMapping(value = "task", consumes = "application/json")
+    @PostMapping(value = "task", consumes = "application/json")
     void receiveTask(@RequestBody Task task, @RequestHeader long userId) {}
 }
