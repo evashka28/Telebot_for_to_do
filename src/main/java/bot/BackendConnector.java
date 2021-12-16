ef2624b557491320ba7c29c325e4b5e9b1676678
distortedroom <nikitos1151@gmail.com>
2021-12-16 11:18:00 +0300

    fixes

--- src/main/java/bot/BackendConnector.java
+++ src/main/java/bot/BackendConnector.java
@@ -6,6 +6,7 @@ import bot.domen.Task;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -156,10 +157,6 @@ public class BackendConnector {
         }
     }
 
-    public Task getTaskByTag(String userId, long tagId) {
-        return null;
-    }
-
     public String addTagToTask(String userId, long taskId, long tagId) {
         ObjectMapper objectMapper = new ObjectMapper();
         URI uri = null;
@@ -285,6 +282,7 @@ public class BackendConnector {
 
     public List<TagRequest> getSchs(String tagId) {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
         URI uri = null;
         try {
             uri = new URI("http://localhost:8081/schedules");
@@ -351,11 +349,46 @@ public class BackendConnector {
         }
     }
 
-    public String deleteSch(String tagId, String Id) {
+    public String setTimezone(String userId, String timezone) {
         ObjectMapper objectMapper = new ObjectMapper();
         URI uri = null;
         try {
-            uri = new URI(String.format("http://localhost:8081/schedule/%s", Id));
+            uri = new URI(String.format("http://localhost:8081/user/%s/timezone", userId));
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        HttpRequest request = HttpRequest.newBuilder(uri)
+                .PUT(HttpRequest.BodyPublishers.ofString(String.format("timezone:%s", timezone)))
+                .header("userId", userId)
+                .build();
+
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient.newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if (response.statusCode() == 410) {
+            return null;
+        } else {
+            String body = response.body();
+            try {
+                return objectMapper.readValue(body, new TypeReference<>() {
+                });
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+
+    public String deleteSch(String tagId, String id) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        URI uri = null;
+        try {
+            uri = new URI(String.format("http://localhost:8081/schedule/%s", id));
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
